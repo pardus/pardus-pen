a8faf4b1f954fe2dc6aabc409c6db5ce6ab103b7
sulincix <parduscix@yandex.ru>
2024-03-06 09:31:07 +0000

    X11 native screenshot

--- src/ScreenShot.cpp
+++ src/ScreenShot.cpp
@@ -1,12 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <QString>
-#include <QStandardPaths>
-#include <QStandardPaths>
-#include <QDateTime>
-#include <QMessageBox>
-
 #include <iostream>
 
 #include "ScreenShot.h"
@@ -18,25 +12,26 @@ extern "C" {
 void takeScreenshot(){
     QString pics = QStandardPaths::writableLocation(QStandardPaths::PicturesLocation);
     QDateTime time = QDateTime::currentDateTime();
-
     QString imgname = pics + "/" + time.toString("yyyy-MM-dd_hh-mm-ss") + ".png";
     int status = 1;
-    std::string spectacle(which((char*)"spectacle"));
-    std::string grim(which((char*)"grim"));
-    std::string gnome_screenshot(which((char*)"gnome-screenshot"));
-    std::string xfce_screenshot(which((char*)"xfce4-screenshooter"));
-    std::string scrot(which((char*)"scrot"));
-    if(strlen(spectacle.c_str()) != 0){
-        status = system(("QT_QPA_PLATFORM='' "+spectacle+" -fbnmo "+imgname.toStdString()).c_str());
-    } else if(strlen(grim.c_str()) != 0){
-        status = system((grim+" -t png "+imgname.toStdString()).c_str());
-    } else if(strlen(gnome_screenshot.c_str()) > 0){
-        status = system((gnome_screenshot+" -f "+imgname.toStdString()).c_str());
-    } else if(strlen(xfce_screenshot.c_str()) > 0){
-        status = system((xfce_screenshot+" -f -s "+imgname.toStdString()).c_str());
-    } else if(strlen(scrot.c_str()) > 0){
-        status = system((scrot+" "+imgname.toStdString()).c_str());
+    // detect X11
+    if (!getenv("WAYLAND_DISPLAY")){
+        QScreen *screen = QGuiApplication::primaryScreen();
+        QPixmap pixmap = screen->grabWindow(0);
+        QFile file(imgname);
+        file.open(QIODevice::WriteOnly);
+        pixmap.save(&file, "PNG");
+        status = 0;
+    } else {
+        std::string spectacle(which((char*)"spectacle"));
+        std::string grim(which((char*)"grim"));
+        if(strlen(spectacle.c_str()) != 0){
+            status = system(("QT_QPA_PLATFORM='' "+spectacle+" -fbnmo "+imgname.toStdString()).c_str());
+        } else if(strlen(grim.c_str()) != 0){
+            status = system((grim+" -t png "+imgname.toStdString()).c_str());
+        }
     }
+    // show message
     QMessageBox messageBox;
     Qt::WindowFlags flags =  Qt::Dialog | Qt::X11BypassWindowManagerHint;
     messageBox.setWindowFlags(flags);
@@ -50,4 +45,5 @@ void takeScreenshot(){
     messageBox.setInformativeText(msg.c_str());
     messageBox.setIcon(QMessageBox::Information);
     messageBox.exec();
+
 }
--- src/ScreenShot.h
+++ src/ScreenShot.h
@@ -1,4 +1,15 @@
 #ifndef _screenshot_h
 #define _screenshot_h
+
+#include <QString>
+#include <QStandardPaths>
+#include <QStandardPaths>
+#include <QDateTime>
+#include <QApplication>
+#include <QScreen>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QFile>
+
 void takeScreenshot();
 #endif
