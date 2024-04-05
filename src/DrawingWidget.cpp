f9b597117eca12e446c4ad771d90c6b39b97e1e9
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2024-04-05 11:18:26 +0000

    clear button event fixx

--- src/DrawingWidget.cpp
+++ src/DrawingWidget.cpp
@@ -55,6 +55,9 @@ public:
 
     void clear(){
         values.clear();
+        image_count = 0;
+        last_image_num = 0;
+        updateGoBackButtons();
     }
 
     QImage loadValue(qint64 id) {
