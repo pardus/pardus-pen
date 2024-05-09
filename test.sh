06eb54522d39fd1c588cb3b3b890baf0ad7b3006
sulincix <parduscix@yandex.ru>
2024-05-09 09:50:47 +0000

    test script update

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ export CXXFLAGS='-g3'
 export GSETTINGS_SCHEMA_DIR=$PWD/data
 glib-compile-schemas ./data
 rm -rf build || true
-meson setup build --prefix=/usr -Dresources=true
+meson setup build --prefix=/usr -Dresources=true "$@"
 ninja -C build
 strip ./build/pardus-pen
 echo -e "run\nbacktrace\n" | gdb ./build/pardus-pen
