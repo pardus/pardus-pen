61fcdc5ce0af2265bf2baa94efe1da761d4c0b68
sulincix <parduscix@yandex.ru>
2024-10-23 08:11:07 +0000

    create appimage added

---
+++ appimage/create_appimage.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+if [ "$QT_PATH" == "" ] ; then
+    export QT_PATH="/usr/lib64/qt5"
+fi
+# cleanup
+rm -rf build || true
+# build
+meson setup build --prefix=/usr -Dresources=true "$@"
+ninja -C build -j`nproc`
+# create dir
+mkdir -p build/appimage/{lib,plugins}
+# install base files
+install build/pardus-pen build/appimage/pardus-pen
+install appimage/AppRun build/appimage/AppRun
+install data/tr.org.pardus.pen.gschema.xml build/appimage/
+install data/tr.org.pardus.pen.svg build/appimage/
+install data/tr.org.pardus.pen.desktop build/appimage/
+# install interpreter
+interpreter=$(ldd build/pardus-pen | grep /ld- | cut -f1 -d" " | tr -d "\t")
+install "$interpreter" build/appimage/$(basename "$interpreter")
+sed -i "s/@interpreter@/$(basename $interpreter)/g" build/appimage/AppRun
+# install qt
+cp -prvf "${QT_PATH}"/plugins/* build/appimage/plugins
+# install libs
+ldd $(find build/appimage -type f) build/pardus-pen | cut -f3 -d" " | sort -V | while read line ; do
+    if [ -f "$line" ] ; then
+        install "$line" build/appimage/lib/$(basename "$line")
+    fi
+done
+# glib compile
+glib-compile-schemas build/appimage/
+# create appimage
+wget https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-$(uname -m).AppImage -O build/appimagetool
+chmod 755 build/appimagetool
+export ARCH=$(uname -m)
+cd build ; ./appimagetool ./appimage
