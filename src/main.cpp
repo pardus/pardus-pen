32ad21612b7934a20b3c5bc8a2cd91623cd9cfd2
sulincix <parduscix@yandex.ru>
2024-12-02 22:10:37 +0000

    fix for qt6 build

--- src/main.cpp
+++ src/main.cpp
@@ -132,8 +132,10 @@ int main(int argc, char *argv[]) {
     }
 
 
+    #ifdef QT5
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    #endif
 
     QCoreApplication::setApplicationName(_("Pardus Pen"));
     QCoreApplication::setOrganizationName("Pardus");
