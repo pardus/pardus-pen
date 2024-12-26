259f7fcb5cef0a4e97a5eda96d867b7810561554
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2024-12-26 15:26:33 +0300

    disable scale for qt6

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,7 @@ int main(int argc, char *argv[]) {
     }
     //Force ignore system dpi
     setenv("QT_AUTO_SCREEN_SCALE_FACTOR", "0", 1);
+    setenv("QT_QT_ENABLE_HIGHDPI_SCALING", "0", 1);
     setenv("QT_SCALE_FACTOR", "1", 1);
 
 
