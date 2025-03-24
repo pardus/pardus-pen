bf3ded1f32877143c82cdf0f6f5466c4c421aed2
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-03-24 11:36:49 +0000

    fix qt6 build

--- src/utils/pdf.cpp
+++ src/utils/pdf.cpp
@@ -1,4 +1,8 @@
+#ifdef QT5
 #include <poppler-qt5.h>
+#else
+#include <poppler-qt6.h>
+#endif
 #include <QImage>
 
 #include "../tools.h"
@@ -6,7 +10,14 @@
 
 bool PDFMODE = false;
 
+#ifdef QT5
 static Poppler::Document *doc;
+static Poppler::Page *page;
+#else
+static std::unique_ptr<Poppler::Document> doc;
+static std::unique_ptr<Poppler::Page> page;
+#endif
+
 void loadPdf(QString path){
     PDFMODE = true;
     doc = Poppler::Document::load(path);
@@ -14,11 +25,10 @@ void loadPdf(QString path){
     doc->setRenderHint(Poppler::Document::TextAntialiasing, true);
 }
 
-static Poppler::Page *page;
 QImage getPdfImage(int num){
     if(doc->numPages() <= num){
         return QImage(0,0);
     }
     page = doc->page(num);
-    return page->renderToImage(72*2*scale, 72*2*scale, 0, 0, page->pageSize().width()*2*scale, page->pageSize().height()*2*scale);
+    return page->renderToImage(72*scale, 72*scale, 0, 0, page->pageSize().width()*scale, page->pageSize().height()*scale);
 }
