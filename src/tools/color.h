aa2ce90e1d7c5a745411932030f30fb28ec05557
sulincix <parduscix@yandex.ru>
2025-04-16 15:41:39 +0000

    update colors

--- src/tools/color.h
+++ src/tools/color.h
@@ -2,36 +2,38 @@
 #define _color_h
 #include <QColor>
 QColor colors[] = {
-    // row 1
-    QColor("#000000"),
-    QColor("#3c4043"),
-    QColor("#5f6368"),
-    QColor("#9aa0a6"),
-    QColor("#dadce0"),
-    QColor("#ffffff"),
-    // row 2
-    QColor("#f28b82"),
-    QColor("#fdd663"),
-    QColor("#81c995"),
-    QColor("#78d9ec"),
-    QColor("#8ab4f8"),
-    QColor("#c58af9"),
-    QColor("#eec9ae"),
-    // row 3
-    QColor("#ea4335"),
-    QColor("#fbbc04"),
-    QColor("#34a853"),
-    QColor("#24c1e0"),
-    QColor("#4285f4"),
-    QColor("#a142f4"),
-    QColor("#e2a185"),
-    // row 4
-    QColor("#c5221f"),
-    QColor("#f29900"),
-    QColor("#188038"),
-    QColor("#12a4af"),
-    QColor("#1967d2"),
-    QColor("#8430ce"),
-    QColor("#885945")
+
+    QColor("#000000"),  // Black
+    QColor("#808080"),  // Gray
+    QColor("#C0C0C0"),  // Silver
+    QColor("#D3D3D3"),  // Light Gray
+    QColor("#FF6347"),  // Tomato
+    QColor("#A52A2A"),   // Brown
+
+    QColor("#800000"),  // Maroon
+    QColor("#FF0000"),  // Red
+    QColor("#FFA500"),  // Orange
+    QColor("#FFFF00"),  // Yellow
+    QColor("#008000"),  // Dark Green
+    QColor("#00FF00"),  // Green
+    QColor("#008080"),   // Dark Cyan
+
+    QColor("#00FFFF"),  // Cyan
+    QColor("#40E0D0"),  // Turquoise
+    QColor("#008B8B"),  // Dark Cyan
+    QColor("#000080"),  // Navy
+    QColor("#800080"),  // Purple
+    QColor("#FF00FF"),  // Magenta
+    QColor("#FFFFFF"),   // White
+
+    QColor("#F0E68C"),  // Khaki
+    QColor("#90EE90"),  // Light Green
+    QColor("#D2691E"),  // Chocolate
+    QColor("#B22222"),  // Firebrick
+    QColor("#FFFF00"),  // Yellow
+    QColor("#FF00FF"),  // Magenta
+    QColor("#0000FF")   // Blue
+
 };
+
 #endif
