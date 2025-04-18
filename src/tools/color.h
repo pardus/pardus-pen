da0c7ba430dee391ff767b7dfbd7f67deabfdf3f
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-04-18 06:49:24 +0000

    sort colors

--- src/tools/color.h
+++ src/tools/color.h
@@ -3,37 +3,33 @@
 #include <QColor>
 QColor colors[] = {
 
+    QColor("#00FF00"),  // Green
+    QColor("#00FFFF"),  // Cyan
+    QColor("#0000FF"),   // Blue
     QColor("#000000"),  // Black
+    QColor("#008B8B"),  // Dark Cyan
+    QColor("#40E0D0"),  // Turquoise
+    QColor("#000080"),  // Navy
+    QColor("#90EE90"),  // Light Green
+    QColor("#008000"),  // Dark Green
+    QColor("#008080"),   // Dark Cyan
+    QColor("#800000"),  // Maroon
+    QColor("#800080"),  // Purple
     QColor("#808080"),  // Gray
+    QColor("#A52A2A"),   // Brown
+    QColor("#B22222"),  // Firebrick
     QColor("#C0C0C0"),  // Silver
     QColor("#D3D3D3"),  // Light Gray
-    QColor("#FF6347"),  // Tomato
-    QColor("#A52A2A"),   // Brown
-
-    QColor("#800000"),  // Maroon
+    QColor("#D2691E"),  // Chocolate
+    QColor("#F0E68C"),  // Khaki
+    QColor("#FF00FF"),  // Magenta
+    QColor("#FF00FF"),  // Magenta
     QColor("#FF0000"),  // Red
+    QColor("#FF6347"),  // Tomato
     QColor("#FFA500"),  // Orange
     QColor("#FFFF00"),  // Yellow
-    QColor("#008000"),  // Dark Green
-    QColor("#00FF00"),  // Green
-    QColor("#008080"),   // Dark Cyan
-
-    QColor("#00FFFF"),  // Cyan
-    QColor("#40E0D0"),  // Turquoise
-    QColor("#008B8B"),  // Dark Cyan
-    QColor("#000080"),  // Navy
-    QColor("#800080"),  // Purple
-    QColor("#FF00FF"),  // Magenta
-    QColor("#FFFFFF"),   // White
-
-    QColor("#F0E68C"),  // Khaki
-    QColor("#90EE90"),  // Light Green
-    QColor("#D2691E"),  // Chocolate
-    QColor("#B22222"),  // Firebrick
     QColor("#FFFF00"),  // Yellow
-    QColor("#FF00FF"),  // Magenta
-    QColor("#0000FF")   // Blue
-
+    QColor("#FFFFFF"),   // White
 };
 
 #endif
