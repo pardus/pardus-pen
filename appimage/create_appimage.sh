782ecec37ffe0d84c0a41cb23987d9c7c8bc0c94
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-06-05 11:47:06 +0300

    fix appimage build

--- appimage/create_appimage.sh
+++ appimage/create_appimage.sh
@@ -2,19 +2,23 @@
 set -e
 if [ "$QT_PATH" == "" ] ; then
     export QT_PATH="/usr/lib64/qt6"
+    if [ ! -d ${QT_PATH} ] ; then
+        export QT_PATH="/usr/lib/$(uname -m)-linux-gnu/qt6/"
+    fi
 fi
 # cleanup
-rm -rf build || true
-# build
-meson setup build --prefix=/usr -Dresources=true "$@"
-ninja -C build -j`nproc`
+rm -rf build/appimage || true
 # create dir
 mkdir -p build/appimage/{lib,plugins}
+# build
+meson setup build --prefix=/ -Dresources=true "$@"
+DESTDIR=$PWD/build/appimage ninja -C build -j`nproc` install
+# remove files
+rm -rf build/appimage/usr
 # install base files
-install build/pardus-pen build/appimage/pardus-pen
+ln -s share/applications/tr.org.pardus.pen.desktop build/appimage/
 install appimage/AppRun build/appimage/AppRun
-install data/tr.org.pardus.pen.svg build/appimage/
-install data/tr.org.pardus.pen.desktop build/appimage/
+install data/tr.org.pardus.pen.svg build/appimage/pardus-pen.svg
 # install interpreter
 interpreter=$(ldd build/pardus-pen | grep /ld- | cut -f1 -d" " | tr -d "\t")
 install "$interpreter" build/appimage/$(basename "$interpreter")
@@ -28,7 +32,7 @@ ldd $(find build/appimage -type f) build/pardus-pen | cut -f3 -d" " | sort -V | 
     fi
 done
 # create appimage
-wget https://github.com/AppImage/appimagetool/releases/download/latest/appimagetool-$(uname -m).AppImage -O build/appimagetool
+wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$(uname -m).AppImage -O build/appimagetool
 chmod 755 build/appimagetool
 export ARCH=$(uname -m)
 cd build ; ./appimagetool ./appimage
