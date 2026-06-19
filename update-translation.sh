99a3c6f65299f39e69ea98069a690d7a5dfe1a86
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-06-19 13:38:36 +0000

    update update-translation.sh

--- update-translation.sh
+++ update-translation.sh
@@ -2,9 +2,8 @@
 set -e
 xgettext -o po/pardus-pen.pot --from-code="utf-8" \
     --language=C++ -v --keyword=_\
-    `find src -type f -iname "*.cpp"`\
-    `find src -type f -iname "*.c"`
-for file in `ls po/*.po`; do
+     -f <(find src -type f -iname "*.cpp" -o -iname "*.c")
+for file in po/*.po; do
     msgmerge $file po/pardus-pen.pot -o $file.new
     echo POT: $file
     rm -f $file
